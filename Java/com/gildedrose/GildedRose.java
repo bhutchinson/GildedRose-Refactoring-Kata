3b4039a58cfc3c1b88ee3cae1c651597913f3fe0
Bryan Hutchinson <bryan@kuali.co>
2015-04-29 14:31:16 -0400

    big cleanup

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -9,119 +9,19 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            final Item item = items[i];
+            Item item = items[i];
 
-            if (item instanceof NormalItem) {
-                ((NormalItem) item).decrementSellIn();
-                ((NormalItem) item).adjustQuality();
-                ((NormalItem) item).adjustQuantityIfPastSellByDate();
-            } else if (item instanceof AgedBrieItem) {
-                ((AgedBrieItem)item).decrementSellIn();
-                ((AgedBrieItem)item).adjustQuality();
-                ((AgedBrieItem)item).adjustQuantityIfPastSellByDate();
-            } else if (item instanceof BackstagePassItem) {
-                ((BackstagePassItem)item).decrementSellIn();
-                ((BackstagePassItem)item).adjustQuality();
-                ((BackstagePassItem)item).adjustQuantityIfPastSellByDate();
-            } else if (item instanceof SulfurusItem) {
-                ((SulfurusItem)item).decrementSellIn();
-                ((SulfurusItem)item).adjustQuality();
-                ((SulfurusItem)item).adjustQuantityIfPastSellByDate();
-            } else if (item instanceof ConjuredItem) {
-                ((ConjuredItem)item).decrementSellIn();
-                ((ConjuredItem)item).adjustQuality();
-                ((ConjuredItem)item).adjustQuantityIfPastSellByDate();
-            } else {
-                decrementSellIn(item);
-                adjustQuality(item);
-                adjustQuantityIfPastSellByDate(item);
+            if ((!(item instanceof AbstractItem))) {
+                item = new NormalItem(item.name, item.sellIn, item.quality);
             }
+            updateItem((AbstractItem)item);
         }
     }
 
-    private void adjustQuality(Item item) {
-        if (isNormalItem(item)) {
-            decrementQualityIfNecessary(item);
-        } else {
-            incrementQuality(item);
-
-            if (isBackstagePass(item)) {
-                incrementBackstagePassQuality(item);
-            }
-        }
+    private void updateItem(AbstractItem item) {
+        item.decrementSellIn();
+        item.adjustQuality();
+        item.adjustQuantityIfPastSellByDate();
     }
 
-    private void decrementSellIn(Item item) {
-        if (!isSulfuras(item)) {
-            item.sellIn = item.sellIn - 1;
-        }
-    }
-
-    private void decrementQualityIfNecessary(Item item) {
-        if (item.quality > 0) {
-            if (isConjuredItem(item)) {
-                item.quality = item.quality - 2;
-            } else if (!isSulfuras(item)) {
-                item.quality = item.quality - 1;
-            }
-        }
-    }
-
-    private void incrementQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private void adjustQuantityIfPastSellByDate(Item item) {
-        if (pastSellByDate(item)) {
-            if (isAgedBrie(item)) {
-                incrementQuality(item);
-            } else {
-                if (isBackstagePass(item)) {
-                    item.quality = 0;
-                } else {
-                    decrementQualityIfNecessary(item);
-                }
-            }
-        }
-    }
-
-    private void incrementBackstagePassQuality(Item item) {
-        if (item.quality < 50) {
-            if (item.sellIn < 10) {
-                incrementQuality(item);
-            }
-
-            if (item.sellIn < 5) {
-                incrementQuality(item);
-            }
-        }
-    }
-
-    private boolean pastSellByDate(Item item) {
-        return item.sellIn < 0;
-    }
-
-    private boolean isNormalItem(Item item) {
-        return !isAgedBrie(item)
-                && !isBackstagePass(item)
-                && !isSulfuras(item);
-    }
-
-    private boolean isConjuredItem(Item item) {
-        return item.name.startsWith("Conjured");
-    }
-
-    private boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
-    }
-
-    private boolean isSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private boolean isBackstagePass(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
 }
